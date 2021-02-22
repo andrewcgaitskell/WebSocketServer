152c0c55cbc64120387a32389ff918cc05ba0061
andrewcgaitskell <andrew@gaitskell.com>
2021-02-22 15:41:11 +0000

    Update 040_pips.sh

--- Build/040_pips.sh
+++ Build/040_pips.sh
@@ -2,33 +2,13 @@ pip install uwsgi
 
 pip install aiohttp_wsgi
 
-pip install pygame
-
-pip install flask
-
-pip install flask-socketio
+pip install aiohttp
 
 pip install gunicorn
 
 pip install eventlet
 
-pip install pandas
 
-pip install flask-bootstrap
-
-pip install sockets
-
-pip install "python-socketio[client]"
-
-pip install "python-socketio[asyncio_client]"
-
-pip install websockets
-
-pip install gevent-websocket
-
-pip install Flask-Sockets
-
-pip install Flask-uWSGI-WebSocket
 
 
 
