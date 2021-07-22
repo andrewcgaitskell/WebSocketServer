c44690dc0b65f016b2a23ccbd9453854acdae841
andrewcgaitskell <andrew@gaitskell.com>
2021-07-22 14:51:30 +0100

    Update client.py

--- aiohttp/socket/client.py
+++ aiohttp/socket/client.py
@@ -1,28 +1,10 @@
+# Echo client program
 import socket
 
-HEADERSIZE = 10
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1241))
-
-while True:
-    full_msg = ''
-    new_msg = True
-    while True:
-        msg = s.recv(16)
-        if new_msg:
-            print("new msg len:",msg[:HEADERSIZE])
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
-
-        print(f"full message length: {msglen}")
-
-        full_msg += msg.decode("utf-8")
-
-        print(len(full_msg))
-
-
-        if len(full_msg)-HEADERSIZE == msglen:
-            print("full msg recvd")
-            print(full_msg[HEADERSIZE:])
-            new_msg = True
+HOST = 'acgtest.com'    # The remote host
+PORT = 5010              # The same port as used by the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+print('Received', repr(data))
