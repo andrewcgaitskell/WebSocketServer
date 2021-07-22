a2c8793f50214be5fe8745cce14f5b740e08ee42
andrewcgaitskell <andrew@gaitskell.com>
2021-07-22 15:37:04 +0100

    Create client.py

---
+++ aiohttp/socket/stream/client.py
@@ -0,0 +1,31 @@
+import pickle
+import socket
+import struct
+
+HEADER_SIZE = 4
+HOST = "127.0.0.1"
+PORT = 12000
+
+
+def receive(nb_bytes, conn):
+    # Ensure that exactly the desired amount of bytes is received
+    received = bytearray()
+    while len(received) < nb_bytes:
+        received += conn.recv(nb_bytes - len(received))
+
+    return received
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+connection, address = s.accept()
+
+while True:
+
+    # receive header
+    header = receive(HEADER_SIZE, connection)
+    data_size = struct.unpack(">i", header)[0] # <-- TypeError
+
+    # receive data
+    data = receive(data_size, connection)
+    print(pickle.loads(data))
