c36bd4017af854fa7a1153548e2fa63837a081c5
andrewcgaitskell <andrew@gaitskell.com>
2021-07-22 14:50:05 +0100

    Update server.py

--- aiohttp/socket/server.py
+++ aiohttp/socket/server.py
@@ -2,7 +2,7 @@
 import socket
 
 HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
+PORT = 5010              # Arbitrary non-privileged port
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
