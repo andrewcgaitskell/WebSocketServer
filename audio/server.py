69cd2e0dc1952a459d992032ec855b9700053ad1
andrewcgaitskell <andrew@gaitskell.com>
2021-07-23 16:43:46 +0100

    Update server.py

--- audio/server.py
+++ audio/server.py
@@ -5,7 +5,7 @@ def on_new_client(clientsocket,addr):
     while True:
         msg = clientsocket.recv(1024)
         #do some checks and if msg == someWeirdSignal: break:
-        print addr, ' >> ', msg
+        print(addr, ' >> ', msg)
         msg = raw_input('SERVER >> ')
         #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
         clientsocket.send(msg)
@@ -15,13 +15,13 @@ s = socket.socket()         # Create a socket object
 host = '' # Get local machine name
 port = 5010                # Reserve a port for your service.
 
-print 'Server started!'
-print 'Waiting for clients...'
+print('Server started!')
+print('Waiting for clients...')
 
 s.bind((host, port))        # Bind to the port
 s.listen(5)                 # Now wait for client connection.
 
-print 'Got connection from', addr
+print('Got connection from', addr)
 while True:
    c, addr = s.accept()     # Establish connection with client.
    thread.start_new_thread(on_new_client,(c,addr))
