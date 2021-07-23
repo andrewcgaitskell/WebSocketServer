3e30f56c6620a159f2d01b9b2a2d5a4c68f0104d
andrewcgaitskell <andrew@gaitskell.com>
2021-07-23 16:21:44 +0100

    Update server.py

--- audio/server.py
+++ audio/server.py
@@ -18,26 +18,26 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     clientsocket, address = s.accept()
-    with conn:
-        print('Connected by', address)
-        full_msg = b''
-        new_msg = True
-        while True:
-            msg = s.recv(16)
-            if new_msg:
-                print("new msg len:",msg[:HEADERSIZE])
-                msglen = int(msg[:HEADERSIZE])
-                new_msg = False
+    print('Connected by', address)
+    
+    full_msg = b''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print("new msg len:",msg[:HEADERSIZE])
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
 
-            print(f"full message length: {msglen}")
+        print(f"full message length: {msglen}")
 
-            full_msg += msg
+        full_msg += msg
 
-            print(len(full_msg))
+        print(len(full_msg))
 
-            if len(full_msg)-HEADERSIZE == msglen:
-                print("full msg recvd")
-                print(full_msg[HEADERSIZE:])
-                print(pickle.loads(full_msg[HEADERSIZE:]))
-                new_msg = True
-                full_msg = b""
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            print(pickle.loads(full_msg[HEADERSIZE:]))
+            new_msg = True
+            full_msg = b""
