23a97e038b478864a8e7fa9ea89e5e107e9b3c86
andrewcgaitskell <andrew@gaitskell.com>
2021-07-24 14:05:13 +0100

    copy of esp32 pickle

---
+++ audio/esp32_pickle/secrets_template.h
@@ -0,0 +1,9 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "SSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "Password"	// replace MyPassword with your WiFi password
+#define SECRET_HOST "host.com"  // replace host.com with your WiFi password
+
+
+#define SECRET_SCRIPT "/macros/s/XXXXXXXXXXXXXXXXXXXXXX/exec"
