d340cb1539a470c29cf596c60b7dddc7c9b1e79a
andrewcgaitskell <andrew@gaitskell.com>
2021-07-27 15:13:29 +0100

    Update threaded_audio_server.py

--- audio/threaded_audio_server.py
+++ audio/threaded_audio_server.py
@@ -50,3 +50,27 @@ def Main():
 	port = 5010
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((host, port))
+
+	print("socket binded to port", port)
+
+	# put the socket into listening mode
+	s.listen(5)
+	print("socket is listening")
+
+	# a forever loop until client wants to exit
+	while True:
+
+		# establish connection with client
+		c, addr = s.accept()
+
+		# lock acquired by client
+		print_lock.acquire()
+		print('Connected to :', addr[0], ':', addr[1])
+
+		# Start a new thread and return its identifier
+		start_new_thread(threaded, (c,))
+	s.close()
+
+	
+if __name__ == '__main__':
+	Main()
