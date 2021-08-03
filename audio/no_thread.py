4f10a8f7aadd4e8ef0e87fb170d4f0099eecc2d9
andrewcgaitskell <andrew@gaitskell.com>
2021-08-03 14:40:54 +0100

    Update no_thread.py

--- audio/no_thread.py
+++ audio/no_thread.py
@@ -58,7 +58,7 @@ def Main():
             #fragments += chunk_byte
             fragments += chunk
             bytes_recd = len(fragments)
-            if bytes_recd > 131072:
+            if bytes_recd > 18384:
                 print(bytes_recd)
                 #break
 
