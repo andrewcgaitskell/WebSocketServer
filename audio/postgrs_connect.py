e961b3d13c0e30b46e7f220f4743898318d3bc0d
andrewcgaitskell <andrew@gaitskell.com>
2021-08-10 11:15:55 +0100

    Create postgrs_connect.py

---
+++ audio/postgrs_connect.py
@@ -0,0 +1,27 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import wget
+import os
+from sqlalchemy import create_engine
+import psycopg2
+
+import os
+
+cwd = os.getcwd()
+sqlfilename = cwd + '/CreateRawTable.sql'
+f = open(sqlfilename, "r")
+fs = f.read()
+
+engine = create_engine('postgresql://pythonuser:pythonuser@localhost:5432/data')
+
+connection = engine.raw_connection()
+try:
+    cursor = connection.cursor()
+    cursor.execute(fs)
+finally:
+    connection.commit()
+    connection.close()
+ 
+sqlcmnd = 'COPY "raw_CovidTrackerGantt" FROM \''+ filename + '\' DELIMITER \',\' CSV;'
+with engine.connect().execution_options(autocommit=True) as con:
+  con.execute(sqlcmnd)
